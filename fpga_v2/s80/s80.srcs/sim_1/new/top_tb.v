006b734f96a4b153182f8766a4a60e9ed9a2e361
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2026-03-19 11:31:30 -0500

    adding testbench

---
+++ fpga_v2/s80/s80.srcs/sim_1/new/top_tb.v
@@ -0,0 +1,31 @@
+// time unit / time precision
+`timescale 1ns / 10ps
+
+module top_tb();
+
+// input signals to the uut
+reg clk = 0;
+reg resetb = 0;
+
+// Generate clock signal
+always
+begin
+    // delay for 10 time units (half a clock period), then change the clock
+    #10
+    clk = ~clk;
+end
+
+// instantiate the uut
+top uut (.clk(clk), .resetb(resetb));
+
+// give it some input
+initial
+begin
+    resetb = 1'b0;
+    #20
+    resetb = 1'b1;
+    #800
+    resetb = 1'b0;
+end
+
+endmodule
