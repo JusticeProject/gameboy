eafdee6558a864ae059ec82b56910ebc04ac8742
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2026-03-25 18:22:54 -0500

    refactoring

--- fpga_v2/s80/s80.srcs/sources_1/new/datapath.v
+++ fpga_v2/s80/s80.srcs/sources_1/new/datapath.v
@@ -40,31 +40,21 @@ module datapath(
 reg [15:0] pc_reg; // the main program counter
 reg [15:0] sp_reg; // the stack pointer
 
-// the registers
-reg [7:0] a_reg;
-reg [7:0] f_reg;
-reg [7:0] f_updates;
-reg [7:0] b_reg;
-reg [7:0] c_reg;
-reg [7:0] d_reg;
-reg [7:0] e_reg;
-reg [7:0] h_reg;
-reg [7:0] l_reg;
+// The registers. _reg is the current value, _next is the value it will get at the next rising edge of the clock
+reg [7:0] a_reg, a_next;
+reg [7:0] f_reg, f_next;
+reg [7:0] b_reg, b_next;
+reg [7:0] c_reg, c_next;
+reg [7:0] d_reg, d_next;
+reg [7:0] e_reg, e_next;
+reg [7:0] h_reg, h_next;
+reg [7:0] l_reg, l_next;
 
-// control signals for loading the registers
-wire ld_a_enable;
-wire ld_af_enable;
-wire update_f_from_alu_op;
-wire ld_b_enable;
-wire ld_c_enable;
-wire ld_d_enable;
-wire ld_e_enable;
-wire ld_h_enable;
-wire ld_l_enable;
-wire ld_pc_enable;
-wire ld_sp_enable;
-
+// input registers that capture the data on mem_data_rd
 reg [7:0] din0, din1;
+
+// next vaue for mem_addr bus
+reg [15:0] mem_addr_next;
 
 // signals to/from the ALU
 wire [15:0] alu_a_in, alu_b_in;
@@ -75,23 +65,30 @@ wire [15:0] alu_out_bus;
 //*************************************************************************************************
 //*************************************************************************************************
 
+// next value for mem_addr bus
+always @*
+begin
+    case (mem_addr_out_enable)
+        `PC_OUT:
+            mem_addr_next = pc_reg;
+        `DE_OUT:
+            mem_addr_next = {d_reg, e_reg};
+        `HL_OUT:
+            mem_addr_next = {h_reg, l_reg};
+        default:
+            mem_addr_next = mem_addr; // by default it will stay the same
+    endcase
+end
+
+//*************************************************************************************************
+
 // output an address onto the mem_addr bus
 always @(negedge resetb, posedge clk)
 begin
     if (!resetb)
         mem_addr <= 16'h0000;
     else
-        begin
-            case (mem_addr_out_enable)
-                `PC_OUT:
-                    mem_addr <= pc_reg;
-                `DE_OUT:
-                    mem_addr <= {d_reg, e_reg};
-                `HL_OUT:
-                    mem_addr <= {h_reg, l_reg};
-                // in the default case the mem_addr bus will remain unchanged, so we don't need to do anything
-            endcase
-        end
+        mem_addr <= mem_addr_next;
 end
 
 //*************************************************************************************************
@@ -132,24 +129,96 @@ begin
         begin
             if (ld_din_enable[0])
                 din0 <= mem_data_rd;
+                
             if (ld_din_enable[1])
                 din1 <= mem_data_rd;
         end
 end
 
 //*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
 
-assign ld_a_enable = ld_reg_enable[`LD_A];
-assign ld_af_enable = ld_reg_enable[`LD_A] && ld_reg_enable[`LD_F];
-assign update_f_from_alu_op = ld_reg_enable[`UPD_F];
-assign ld_b_enable = ld_reg_enable[`LD_B];
-assign ld_c_enable = ld_reg_enable[`LD_C];
-assign ld_d_enable = ld_reg_enable[`LD_D];
-assign ld_e_enable = ld_reg_enable[`LD_E];
-assign ld_h_enable = ld_reg_enable[`LD_H];
-assign ld_l_enable = ld_reg_enable[`LD_L];
-assign ld_pc_enable = ld_reg_enable[`LD_PC];
-assign ld_sp_enable = ld_reg_enable[`LD_SP];
+// next value for af, use flags from the ALU
+always @*
+begin
+    a_next = a_reg; // defaults are set to the current value
+    f_next = f_reg;
+    
+    if (ld_reg_enable[`UPD_F])
+        begin
+            // the flags are ZNHC----
+            if (z_flag_enable)
+                f_next[7] = z_flag_next;
+                
+            if (ld_reg_enable[`LD_A])
+                a_next = alu_out_bus[7:0];
+        end
+    else if (ld_reg_enable[`LD_A] && ld_reg_enable[`LD_F])
+        begin
+            a_next = alu_out_bus[15:8];
+            f_next = alu_out_bus[7:0];
+        end
+    else if (ld_reg_enable[`LD_A])
+        a_next = alu_out_bus[7:0];
+end
+
+//*************************************************************************************************
+
+// next value for bc
+always @*
+begin
+    b_next = b_reg; // defaults are set to the current value
+    c_next = c_reg;
+
+    if (ld_reg_enable[`LD_B] && ld_reg_enable[`LD_C])
+        begin
+            b_next = alu_out_bus[15:8];
+            c_next = alu_out_bus[7:0];
+        end
+    else if (ld_reg_enable[`LD_B])
+        b_next = alu_out_bus[7:0];
+    else if (ld_reg_enable[`LD_C])
+        c_next = alu_out_bus[7:0];
+end
+
+//*************************************************************************************************
+
+// next value for de
+always @*
+begin
+    d_next = d_reg; // defaults are set to the current value
+    e_next = e_reg;
+    
+    if (ld_reg_enable[`LD_D] && ld_reg_enable[`LD_E])
+        begin
+            d_next = alu_out_bus[15:8];
+            e_next = alu_out_bus[7:0];
+        end
+    else if (ld_reg_enable[`LD_D])
+        d_next = alu_out_bus[7:0];
+    else if (ld_reg_enable[`LD_E])
+        e_next = alu_out_bus[7:0];
+end
+
+//*************************************************************************************************
+
+// next value for hl
+always @*
+begin
+    h_next = h_reg; // defaults are set to the current value
+    l_next = l_reg;
+    
+    if (ld_reg_enable[`LD_H] && ld_reg_enable[`LD_L])
+        begin
+            h_next = alu_out_bus[15:8];
+            l_next = alu_out_bus[7:0];
+        end
+    else if (ld_reg_enable[`LD_H])
+        h_next = alu_out_bus[7:0];
+    else if (ld_reg_enable[`LD_L])
+        l_next = alu_out_bus[7:0];
+end
 
 //*************************************************************************************************
 
@@ -169,69 +238,15 @@ begin
         end
     else
         begin
-            if (ld_af_enable)
-                begin
-                    a_reg <= alu_out_bus[15:8];
-                    f_reg <= alu_out_bus[7:0];
-                end
-            else if ((ld_a_enable) && (update_f_from_alu_op))
-                begin
-                    a_reg <= alu_out_bus[7:0];
-                    f_reg <= f_updates;
-                end
-            else if (ld_a_enable)
-                a_reg <= alu_out_bus[7:0];
-            else if (update_f_from_alu_op)
-                f_reg <= f_updates;
-            
-            
-            
-            if (ld_b_enable && ld_c_enable)
-                begin
-                    b_reg <= alu_out_bus[15:8];
-                    c_reg <= alu_out_bus[7:0];
-                end
-            else if (ld_b_enable)
-                b_reg <= alu_out_bus[7:0];
-            else if (ld_c_enable)
-                c_reg <= alu_out_bus[7:0];
-            
-            
-            
-            if (ld_d_enable && ld_e_enable)
-                begin
-                    d_reg <= alu_out_bus[15:8];
-                    e_reg <= alu_out_bus[7:0];
-                end
-            else if (ld_d_enable)
-                d_reg <= alu_out_bus[7:0];
-            else if (ld_e_enable)
-                e_reg <= alu_out_bus[7:0];
-            
-            
-            
-            if (ld_h_enable && ld_l_enable)
-                begin
-                    h_reg <= alu_out_bus[15:8];
-                    l_reg <= alu_out_bus[7:0];
-                end
-            else if (ld_h_enable)
-                h_reg <= alu_out_bus[7:0];
-            else if (ld_l_enable)
-                l_reg <= alu_out_bus[7:0];
+            a_reg <= a_next;
+            f_reg <= f_next;
+            b_reg <= b_next;
+            c_reg <= c_next;
+            d_reg <= d_next;
+            e_reg <= e_next;
+            h_reg <= h_next;
+            l_reg <= l_next;
         end
-end
-
-//*************************************************************************************************
-
-// update the flags with signals from the ALU
-always @(z_flag_enable, f_reg, z_flag_next)
-begin
-    f_updates = f_reg; // set the default
-    
-    // the flags are ZNHC----
-    if (z_flag_enable)
-        f_updates[7] = z_flag_next;
 end
 
 //*************************************************************************************************
@@ -240,8 +255,10 @@ end
 assign z_flag_reg = f_reg[7];
 
 //*************************************************************************************************
+//*************************************************************************************************
+//*************************************************************************************************
 
-// load the PC
+// load the PC and SP
 always @(negedge resetb, posedge clk)
 begin
     // TODO: need to start at h0100 for GameBoy ROM
@@ -252,10 +269,10 @@ begin
         end
     else 
         begin
-            if (ld_pc_enable)
+            if (ld_reg_enable[`LD_PC])
                 pc_reg <= alu_out_bus;
             
-            if (ld_sp_enable)
+            if (ld_reg_enable[`LD_SP])
                 sp_reg <= alu_out_bus;
         end
 end
